6d9e5b30b7e3036afd292bb99a7c272a6c9a0345
Tom <tombrwe@gmail.com>
2017-04-15 20:38:53 +0200

    package cleanup

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-import 'aframe';
+// import Vue from 'vuuu';
 import jsHue from './jshue';
-
+import Vue from 'vue';
+var lightList = document.getElementById('lightlist');
 
 var hue = jsHue();
-var bridge = hue.bridge('192.168.2.17');
+var bridge = hue.bridge('192.168.1.17');
 var user = bridge.user('lampe-bash');
 
+let app = new Vue({
+  el: '#app',
+  data: {
+    lights: [],
+		foo: 'bar'
+  },
+  methods: {
 
-user.getConfig().then(config => {console.log(config)}).catch((err) => {console.log(err);})
+  }
+})
 
-user.getLight(1).then((state) => {console.log(state);});
-user.getLights().then((lights) => {console.log(lights);})
+
+// Starts the setup in which the user can place virtual lights
+// in his room with the VR controllers.
+function startLightSetup() {
+
+}
+
+// 
+// function init() {
+// 	user.getConfig().then(config => {console.log(config)}).catch((err) => {console.log(err);})
+// 	app.foo = 'blub';
+// 	user.getLights().then((_lights) => {
+// 		if(Object.keys(_lights).length === 0) {
+// 			console.error('No lights found!');
+// 		}
+// 		console.log(_lights);
+// 		// app.lights = _lights;
+//
+// 	})
+// }
+
+init();
+setTimeout(() => {
+	startLightSetup()
+}, 10000);
--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,39 @@
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import builtins from 'rollup-plugin-node-builtins';
+import globals from 'rollup-plugin-node-globals';
+import alias from 'rollup-plugin-alias';
 
 export default {
   plugins: [
+    alias({
+      'vue': 'node_modules/vue/dist/vue.esm.js'
+    }),
+    globals(),
+    builtins(),
     babel(),
     nodeResolve({
-      jsnext: true,
-      main: true
+
     }),
     commonjs({
         // non-CommonJS modules will be ignored, but you can also
         // specifically include/exclude files
-        include: 'node_modules/**',  // Default: undefined
-
+        
+        
         // if true then uses of `global` won't be dealt with by this plugin
-        ignoreGlobal: false,  // Default: false
+        ignoreGlobal: true,  // Default: false
 
         // if false then skip sourceMap generation for CommonJS modules
-        sourceMap: true  // Default: true
+        sourceMap: true,  // Default: true
+        
+        namedExports: {
+          // left-hand side can be an absolute path, a path
+          // relative to the current directory, or the name
+          // of a module in node_modules
+        }
+      }),
 
-        // explicitly specify unresolvable named exports
-        // (see below for more details)
-        // namedExports: { './module.js': ['foo', 'bar' ] }  // Default: undefined
-      })
     ],
     
   entry: 'index.js',
